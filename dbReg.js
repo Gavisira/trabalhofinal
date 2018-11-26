248b8ef7e3e91e7eefe5ac1ceafea4ede25b289f
Gavisira <42875966+Gavisira@users.noreply.github.com>
2018-11-26 19:37:13 -0200

    Update dbReg.js

--- dbReg.js
+++ dbReg.js
@@ -67,7 +67,7 @@ function updateEspecie(id, animal) {
     localStorage.setItem('db', JSON.stringify(db));
 }
 
-function deleteespecie(id) {    
+function deleteEspecie(id) {    
     // Filtra o array removendo o elemento com o id passado
     db.data = db.data.filter(function (element) { return element.id != id });
 
